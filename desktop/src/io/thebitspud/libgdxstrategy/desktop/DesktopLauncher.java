05afb83c2b324a661b61d2d58f03fc9cd7cef2a4
James Zhong <thebitspud@gmail.com>
2020-08-29 22:30:46 -0700

    Generated base LibGDX project

---
+++ desktop/src/io/thebitspud/libgdxstrategy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.thebitspud.libgdxstrategy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.thebitspud.libgdxstrategy.StrategyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StrategyGame(), config);
+	}
+}
